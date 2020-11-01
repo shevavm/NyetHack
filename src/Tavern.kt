e4f88c949bf298d58dadb1943ebaa06972835123
shevavm <shevavm@gmail.com>
2020-11-01 07:15:05 +0200

    Tavern + робота з String. p.7

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,3 +1,5 @@
+const val TAVERN_NAME = "Taernyl's Folly"//7.1
+
 //6.1
 fun main(args: Array <String>) {
     /*6.2 var signatureDrink = "Buttered Ale"
@@ -11,7 +13,7 @@ fun main(args: Array <String>) {
         }
     } //6.7 let */
     //var beverage = readLine()!!.capitalize() //6.8 !! 6.9
-    var beverage = readLine()
+    /*var beverage = readLine()
 
 // 6.4 Ctrl+/    beverage = null //6.3
     //beverage = null //6.7
@@ -22,5 +24,43 @@ fun main(args: Array <String>) {
     } //6.8 6.9
 //    println(beverage) 6.10
     val beverageServed: String = beverage ?: "Buttered Ale"
-    println(beverageServed)
+    println(beverageServed)*/
+    //7.7 placeOrder("shandy, Dragon's Breath, 5.91")
+    placeOrder("elixir, Shirley's Temple, 4.12")
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    /*val data = menuData.split(',')//7.3
+    val type = data [0]
+    val name = data [1]
+    val price = data [2]*/
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    /*7.6 val phrase = "Ah, delicious $name!"
+    println("Madrigal exclamims: ${toDragonSpeak(phrase)}")*/
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Tranks for the $name."
+    }
+    println(phrase)
 }
